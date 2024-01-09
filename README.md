13216f22216e24eaa638b3b71e730d1c5f61b462
FirstLearnJava <mat.floimayr@derflorianer.at>
2024-01-09 12:45:27 +0100

    Add all basic requirements

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 It should allow the user to:
 
-- [ ] Enter text for the top and bottom of the meme
-  - [ ] The top text box needs to have a label element associated with it containing the text Top text
-  - [ ] The bottom text box needs to have a label element associated with it with the text Bottom text
-  - [ ] Both text boxes should be empty when the page first loads
-- [ ] Preview the generated meme
-  - [ ] The image element needs to have an html attribute set as follows: data-test-id="meme-image"
-    - [ ] This image element should show a working image when the page first loads
-- [ ] Change the meme template (the background image)
-  - [ ] The meme template selector element needs to have a label element associated with it containing the text Meme template
-  - [ ] If the user follows the steps below, the doge meme template needs to be selected:
-    - [ ] Click on the label of the meme template selector
-    - [ ] Clear any existing value (eg. with a text box)
-    - [ ] Type the text doge
-    - [ ] Hit enter
+- [x] Enter text for the top and bottom of the meme
+  - [x] The top text box needs to have a label element associated with it containing the text Top text
+  - [x] The bottom text box needs to have a label element associated with it with the text Bottom text
+  - [x] Both text boxes should be empty when the page first loads
+- [x] Preview the generated meme
+  - [x] The image element needs to have an html attribute set as follows: data-test-id="meme-image"
+    - [x] This image element should show a working image when the page first loads
+- [x] Change the meme template (the background image)
+  - [x] The meme template selector element needs to have a label element associated with it containing the text Meme template
+        If the user follows the steps below, the doge meme template needs to be selected:
+    - [x] Click on the label of the meme template selector
+    - [x] Clear any existing value (eg. with a text box)
+    - [x] Type the text doge
+    - [x] Hit enter
 - [ ] Download the meme by clicking on a button
   - [ ] The button element needs to contain the text Download
 
--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,43 +1,97 @@
 import React, { useState, useEffect } from 'react';
-//import * as cheerio from 'cheerio';
-//import axios from 'axios';
+import { saveAs } from 'file-saver';
 
-export default function MemeGenerator() {
+export default function MemeGenerator(props) {
   const [memeUrls, setMemesUrls] = useState([]);
+  const [memeIds, setMemeIds] = useState([]);
+  const [memesArray, setMemesArray] = useState([]);
   const [randomUrl, setRandomUrl] = useState();
+  const [pngUrl, setPngUrl] = useState('');
+  const [mathRandom, setMathRandom] = useState(Math.random());
+  const [memeTemplateUrl, setMemeTemplateUrl] = useState();
+  const [currentId, setCurrentId] = useState();
 
   useEffect(() => {
     async function fetchMemes() {
       const response = await fetch('https://api.memegen.link/templates');
       const memes = await response.json();
       const mappedUrls = memes.map((meme) => meme.blank);
+      const mappedIds = memes.map((meme) => meme.id);
+      setMemesArray(memes);
+      setMemeIds(mappedIds);
       setMemesUrls(mappedUrls);
     }
-
     fetchMemes();
   }, []);
-
   useEffect(() => {
     function getRandomUrl() {
-      setRandomUrl(memeUrls[Math.floor(Math.random() * memeUrls.length)]);
+      const fetchedUrl = memeUrls[Math.floor(mathRandom * memeUrls.length)];
+      const correspondingId = memeIds[Math.floor(mathRandom * memeIds.length)];
+      //setPngUrl(fetchedUrl);
+      if (memeTemplateUrl) {
+        setPngUrl(memeTemplateUrl);
+      } else {
+        setPngUrl(fetchedUrl);
+        setCurrentId(correspondingId);
+      }
+      /*       if (correspondingId) {
+
+      } */
     }
     getRandomUrl();
-  }, [memeUrls]);
+  }, [memeUrls, mathRandom, memeTemplateUrl]);
 
-  /*   function setRandomImage() {
-    setRandomUrl(memeUrls[Math.floor(Math.random() * memeUrls.length)]); */
+  useEffect(() => {
+    if (pngUrl) {
+      function changeUrlToJpgFormat() {
+        const jpgUrl = `${pngUrl.split('.png').join(``)}`;
+        const captionedUrl = `${jpgUrl}${props.topCaption ? '/' : ''}${
+          props.topCaption
+        }${props.bottomCaption ? '/' : ''}${props.bottomCaption}.jpg`;
+        setRandomUrl(captionedUrl);
+      }
+      changeUrlToJpgFormat();
+    }
+  }, [pngUrl, props.topCaption, props.bottomCaption]);
+
+  /* function downloadImage() {
+    FileSaver.saveAs(randomUrl, `${currentId}.jpg`);
+  } */
 
   return (
     <>
       <div>Meme Generator</div>
-      <img src={randomUrl} height="200" width="250"></img>
+      <img
+        src={randomUrl}
+        height="400"
+        width="460"
+        data-test-id="meme-image"
+      ></img>
+      <button onClick={() => setMathRandom(Math.random())}>
+        Select random meme
+      </button>
+      <label for="meme_template">
+        Meme template
+        <input
+          id="meme-template"
+          placeholder={memeIds[Math.floor(mathRandom * memeIds.length)]}
+          onChange={(e) => {
+            let userInput = e.currentTarget.value;
+            const obj = memesArray.find((o) => o.id === userInput);
+            if (obj) {
+              setMemeTemplateUrl(obj.blank);
+              setCurrentId(obj.id);
+            }
+          }}
+        ></input>
+      </label>
       <button
-        width="50"
-        height="30"
-        onClick={() =>
-          setRandomUrl(memeUrls[Math.floor(Math.random() * memeUrls.length)])
-        }
-      ></button>
+        onClick={() => {
+          saveAs(randomUrl, `${currentId}.jpg`);
+        }}
+      >
+        Download
+      </button>
     </>
   );
 }
